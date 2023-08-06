6ca462fa9a4423b1a436b2a5faf56739ec5a44a4
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-08-06 17:07:35 +0300

    Initial Commit

---
+++ src/main/java/ru/Netology/NotFoundException.java
@@ -0,0 +1,7 @@
+package ru.Netology;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(int id) {
+        super("Товар с ID = "+ id+ " не найден");
+    }
+}
